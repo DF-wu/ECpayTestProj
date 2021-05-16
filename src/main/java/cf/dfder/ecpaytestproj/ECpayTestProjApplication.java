6e4ead9f2cdb3b9f8697fe71e4c7e4d7f92d7e42
dfder <a2470abc@gmail.com>
2021-05-16 17:23:54 +0800

    init

---
+++ src/main/java/cf/dfder/ecpaytestproj/ECpayTestProjApplication.java
@@ -0,0 +1,14 @@
+package cf.dfder.ecpaytestproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECpayTestProjApplication {
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ECpayTestProjApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/cf/dfder/ecpaytestproj/ECpayTestProjApplicationTests.java
@@ -0,0 +1,14 @@
+package cf.dfder.ecpaytestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECpayTestProjApplicationTests {
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
