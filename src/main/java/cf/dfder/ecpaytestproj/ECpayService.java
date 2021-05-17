5f3f286e8ec7fa9a071d3255cd2b24606c9b0386
dfder <a2470abc@gmail.com>
2021-05-17 17:10:14 +0800

    add init func

--- src/main/java/cf/dfder/ecpaytestproj/ECpayService.java
+++ src/main/java/cf/dfder/ecpaytestproj/ECpayService.java
@@ -1,22 +1,63 @@
 package cf.dfder.ecpaytestproj;
 
+import cf.dfder.ecpaytestproj.entity.Order;
+import cf.dfder.ecpaytestproj.repository.OrderRepo;
 import ecpay.payment.integration.AllInOne;
 import ecpay.payment.integration.domain.AioCheckOutALL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.UUID;
+
 @Service
 public class ECpayService {
+    private OrderRepo orderrepo;
+    
     private AllInOne ecpaySDK;
+    private int lastSerialNumber;
     
-    
-    public ECpayService(){
+    @Autowired
+    public ECpayService(OrderRepo orderrepo){
+        this.orderrepo = orderrepo;
+        lastSerialNumber = 0;
         ecpaySDK = new AllInOne("");
     }
     
-    public String checkOut(){
+    
+    public String init(){
+        Order order = new Order();
+        order.setSerialNumber(10);
+        
         AioCheckOutALL obj = new AioCheckOutALL();
-        obj.setMerchantTradeNo("testCompany0004");
+        UUID uuid = UUID.randomUUID();
+        obj.setMerchantTradeNo(uuid.toString().substring(0,18).replace("-", ""));
+        obj.setMerchantTradeDate("2017/01/01 08:05:23");
+        obj.setTotalAmount("50");
+        obj.setTradeDesc("test Description");
+        obj.setItemName("TestItem");
+        obj.setReturnURL("http://211.23.128.214:5000");
+        obj.setNeedExtraPaidInfo("N");
+        System.out.println(uuid);
+        order.setAioCheckOutALL(obj);
+        orderrepo.save(order);
+        
+        String form = ecpaySDK.aioCheckOut(obj, null);
+        
+        System.out.println("init order");
+        return form;
+    }
+    
+    public String checkOut(){
+        
+        Order order = new Order();
+        lastSerialNumber++;
+        order.setSerialNumber(lastSerialNumber);
+        
+        AioCheckOutALL obj = new AioCheckOutALL();
+        obj.setMerchantTradeNo(String.valueOf(lastSerialNumber));
         obj.setMerchantTradeDate("2017/01/01 08:05:23");
         obj.setTotalAmount("50");
         obj.setTradeDesc("test Description");
@@ -25,8 +66,31 @@ public class ECpayService {
         obj.setNeedExtraPaidInfo("N");
         String form = ecpaySDK.aioCheckOut(obj, null);
     
+        orderrepo.save(order);
+        
         System.out.println("from check out service" + form);
         return form;
+    }
+    
+    
+    public String getLastSerialNumber(){
+        ArrayList<Order> orders = new ArrayList<>();
+        orders = (ArrayList<Order>) orderrepo.findAll();
+    
+    
+        Comparator<Order> comparator = new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2)
+            {
+                return o1.getSerialNumber() - o2.getSerialNumber();
+            }
+        };
+        Collections.sort(orders,comparator);
+    
+    
+        System.out.println(orders);
+        
+        return orders.get(0).toString();
     }
     
     
